ebcd4233ecd1f3be244316fb4c4199c86a522270
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-09-22 10:51:43 +0200

    Added the math functions

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 let firstValue = parseInt(prompt("Type the first number"));
-let operator = prompt("Type the operator");
-let secondValue = parseInt(prompt("Type the second number"));
-
 console.log(firstValue);
+let operator = prompt("Type the operator");
 console.log(operator);
+let secondValue = parseInt(prompt("Type the second number"));
 console.log(secondValue);
-let result = firstValue + secondValue;
-console.log(result);
+
+function sum (a,b){
+  return a+b;
+}
+function substraction (a,b){
+  return a-b;
+}
+function multiply (a,b){
+  return a*b;
+}
+function division (a,b){
+  return a/b;
+}
+function modulo (a,b){
+  return a%b;
+}
+function result (){
+    if (operator == "+"){
+      console.log(sum(firstValue, secondValue));
+    }
+    else if (operator == "-" ){
+      console.log(substraction(firstValue, secondValue));
+    }
+    else if (operator == "*"){
+      console.log(multiply(firstValue, secondValue));
+    }
+    else if (operator == "/" ){
+      console.log(division(firstValue, secondValue));
+    }
+    else if (operator == "%"){
+      console.log(modulo(firstValue, secondValue));
+    }
+    else{
+      console.log("Not a operator!!!")
+    }
+}
+
+result()
