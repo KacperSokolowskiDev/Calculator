f458cea8d555dbb2cd7a114ea56e99d8f77e9323
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-09-22 10:33:22 +0200

    New script file and a new h1 title added

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstValue = parseInt(prompt("Type the first number"));
+let operator = prompt("Type the operator");
+let secondValue = parseInt(prompt("Type the second number"));
+
+console.log(firstValue);
+console.log(operator);
+console.log(secondValue);
+let result = firstValue + secondValue;
+console.log(result);
